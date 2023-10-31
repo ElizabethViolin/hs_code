1099679b4d40cba2fe20f96098c67ff1dbb93dda
Elizabeth Taylor <elizabethtaylor@Elizabeths-MacBook-Pro-2.local>
2023-10-30 21:21:28 -0400

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+// pages/page.js (or the actual filename)
+import React from 'react';
+import SearchBar from './components/SearchBar';
+import DataTable from './components/DataTable';
+// import { sendRequest } from '../utilities/api';
+
+const Home = () => {
+  // You can use sendRequest here if needed
+  return (
+    <div>
+      <SearchBar />
+      <DataTable />
+    </div>
+  );
+};
+
+export default Home;
+
